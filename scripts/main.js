4b92bd581225ffa61397f1bc5b96d4170e2f53b8
hirszerzesHarcos <hirszerzesharcos@gmail.com>
2025-11-21 23:59:54 +0100

    A localStorage teljes törlésére gomb hozzáadva.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 "use strict";
 const saveBtn = document.getElementById("save");
 const loadValueBtn = document.getElementById("loadValue");
+const deleteBtn = document.getElementById("delete");
 if (saveBtn) {
     saveBtn.addEventListener("click", function () {
         localStorage.value = document.getElementsByTagName("input")[0].value;
@@ -11,5 +12,10 @@ if (loadValueBtn) {
         const inputElement = document.getElementsByTagName("input")[0];
 
         inputElement.value = localStorage.getItem("value");
+    })
+}
+if (deleteBtn) {
+    deleteBtn.addEventListener("click", function () {
+        localStorage.clear();
     })
 }
